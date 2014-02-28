451242b16fda4f1a7b6d64499117aaf96007409f
wwwcomy <wwwcomy@gmail.com>
2014-02-27 18:29:26 -0800

    添加子订单出库的界面显示和页面跳转

---
+++ haiyanV1/tableconfig_leedon/project/leedon/V_WM_OUT3.js
@@ -0,0 +1,11 @@
+﻿
+Hy.UIFunction.getsuborderid=function() {
+	var paras=Hy.getQueryParams().split('&');
+	var result;
+	paras.each(function(item,index){
+		var tmp = item.split("=");
+		if(tmp[0]=="SUBORDER_ID")
+			result=tmp[1];
+	});
+	return result;
+};
