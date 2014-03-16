0dcba836805345ce6c44af792c9e523a7d275d87
wwwcomy <wwwcomy@gmail.com>
2014-03-16 18:01:22 +0800

    继承的修饰符和异常的广度

--- src/main/java/com/iteye/wwwcomy/Me.java
+++ src/main/java/com/iteye/wwwcomy/Me.java
@@ -4,4 +4,23 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println(Me.class.getName().replaceAll(".", "/") + ".class");
 	}
+	
+	protected int test() throws RuntimeException{
+		return 0;
+	}
+}
+
+/**
+ * 继承 范围修饰符可以大  抛出的异常必须小
+ * 
+ * @author wwwcomy
+ *
+ */
+class I extends Me{
+	public int test() throws IndexOutOfBoundsException{
+		return 0;
+	}
+	
+	private native void test1();
+	synchronized public void test2(){};
 }
