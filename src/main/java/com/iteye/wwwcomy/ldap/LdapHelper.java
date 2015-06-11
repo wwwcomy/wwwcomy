5af3a3d408f39af1526076ae7bd9f5f5dd26de35
wwwcomy@gmail.com <wwwcomy@gmail.com>
2015-06-11 16:41:22 +0800

    Add LDAP Support API and some test cases.

---
+++ src/main/java/com/iteye/wwwcomy/ldap/LdapHelper.java
@@ -0,0 +1,118 @@
+package com.iteye.wwwcomy.ldap;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
+/**
+ * @author liuxingn
+ *
+ */
+@SuppressWarnings("restriction")
+public class LdapHelper {
+
+	private static LdapContext ctx;
+
+	@SuppressWarnings(value = { "unchecked", "rawtypes" })
+	public static LdapContext getCtx() {
+		if (ctx == null) {
+			synchronized (LdapHelper.class) {
+				if (ctx == null) {
+					String account = "cn=person2,o=organization1"; // binddn
+					String password = "1"; // bindpwd
+
+					account = "cn=Manager"; // binddn
+					password = "1"; // bindpwd
+
+					String root = "dc=my-domain,dc=com"; // root
+					Hashtable env = new Hashtable();
+					env.put(Context.INITIAL_CONTEXT_FACTORY,
+							"com.sun.jndi.ldap.LdapCtxFactory");
+					env.put(Context.PROVIDER_URL, "ldap://localhost:389/"
+							+ root);
+					env.put(Context.SECURITY_AUTHENTICATION, "simple");
+					// This account should be followed by root DN
+					env.put(Context.SECURITY_PRINCIPAL, account + "," + root);
+					env.put(Context.SECURITY_CREDENTIALS, password);
+					try {
+						// 链接ldap
+						ctx = new InitialLdapContext(env, null);
+						System.out.println("认证成功");
+					} catch (javax.naming.AuthenticationException e) {
+						System.out.println("认证失败,密码输入错误");
+						e.printStackTrace();
+					} catch (Exception e) {
+						System.out.println("认证出错：");
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return ctx;
+	}
+
+	public static void closeCtx() {
+		try {
+			ctx.close();
+		} catch (NamingException ex) {
+			Logger.getLogger(LdapHelper.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+	}
+
+	public static boolean verifySHA(String ldappw, String inputpw)
+			throws NoSuchAlgorithmException {
+
+		// MessageDigest 提供了消息摘要算法，如 MD5 或 SHA，的功能，这里LDAP使用的是SHA-1
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+		// 取出加密字符
+		if (ldappw.startsWith("{SSHA}")) {
+			ldappw = ldappw.substring(6);
+		} else if (ldappw.startsWith("{SHA}")) {
+			ldappw = ldappw.substring(5);
+		}
+
+		// 解码BASE64
+		byte[] ldappwbyte = Base64.decode(ldappw);
+		byte[] shacode;
+		byte[] salt;
+
+		// 前20位是SHA-1加密段，20位后是最初加密时的随机明文
+		if (ldappwbyte.length <= 20) {
+			shacode = ldappwbyte;
+			salt = new byte[0];
+		} else {
+			shacode = new byte[20];
+			salt = new byte[ldappwbyte.length - 20];
+			System.arraycopy(ldappwbyte, 0, shacode, 0, 20);
+			System.arraycopy(ldappwbyte, 20, salt, 0, salt.length);
+		}
+
+		// 把用户输入的密码添加到摘要计算信息
+		md.update(inputpw.getBytes());
+		// 把随机明文添加到摘要计算信息
+		md.update(salt);
+
+		// 按SSHA把当前用户密码进行计算
+		byte[] inputpwbyte = md.digest();
+
+		// 返回校验结果
+		return MessageDigest.isEqual(shacode, inputpwbyte);
+	}
+
+	public static void main(String[] args) throws Exception {
+		getCtx();
+		// LdapContext instance = ctx.newInstance(null);
+	}
+
+}
---
+++ src/main/java/com/iteye/wwwcomy/ldap/LdapTest.java
@@ -0,0 +1,170 @@
+package com.iteye.wwwcomy.ldap;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author xingnan.liu
+ *
+ */
+public class LdapTest {
+	@Test
+	public void canAdd() throws Exception {
+		LdapContext ctx = LdapHelper.getCtx();
+		LdapContext newCtx = ctx.newInstance(null);
+		Attributes attrs = new BasicAttributes(true);
+		Attribute objclass = new BasicAttribute("objectclass");
+		// 添加ObjectClass
+		String[] attrObjectClassPerson = { "organizationalPerson",
+				"inetOrgPerson", "person", "top" };
+		Arrays.sort(attrObjectClassPerson);
+		for (String ocp : attrObjectClassPerson) {
+			objclass.add(ocp);
+		}
+
+		attrs.put(objclass);
+		String uid = "zhangsan";
+		// 这里加上root会报错
+		String userDN = "uid=" + uid + "," + "o=org2";
+		// 密码处理
+		// attrs.put("uid", uid);
+		attrs.put("cn", uid);
+		attrs.put("sn", uid);
+		attrs.put("uid", uid);
+		attrs.put("displayName", "张三");
+		attrs.put("mail", "zhangsan@163.com");
+		// attrs.put("description", ""); // 如果设置属性为空貌似会报错
+		attrs.put("userPassword", "zhangsan".getBytes("UTF-8"));
+		newCtx.createSubcontext(userDN, attrs);
+		newCtx.close();
+	}
+
+	@Test
+	public void canDel() throws Exception {
+		LdapContext ctx = LdapHelper.getCtx();
+		LdapContext newCtx = ctx.newInstance(null);
+		String uid = "zhangsan";
+		String userDN = "uid=" + uid + "," + "o=org2";
+		newCtx.destroySubcontext(userDN);
+		newCtx.close();
+	}
+
+	@Test
+	public void canModifyAttr() throws Exception {
+		LdapContext ctx = LdapHelper.getCtx();
+		LdapContext newCtx = ctx.newInstance(null);
+		String uid = "zhangsan";
+		String userDN = "uid=" + uid + "," + "o=org2";
+		Attributes attrs = new BasicAttributes(true);
+		attrs.put("mail", "zhangsanModify@163.com");
+		// 使用 DirContext.REMOVE_ATTRIBUTE 或ADD_ATTRIBUTE 来添加、删除Attribute
+		// 可以使用ModificationItem的数组来传递attributes
+		// 可以通过这种方式来修改密码
+		newCtx.modifyAttributes(userDN, DirContext.REPLACE_ATTRIBUTE, attrs);
+		newCtx.close();
+	}
+
+	@Test
+	public void canSearch() throws Exception {
+		LdapContext ctx = LdapHelper.getCtx();
+		LdapContext newCtx = ctx.newInstance(null);
+		// 设置过滤条件
+		String uid = "zhangsan";
+		String filter = "(&(objectClass=top)(objectClass=organizationalPerson)(uid="
+				+ uid + "))";
+		filter = "objectClass=top";
+
+		// 限制要查询的字段内容
+		String[] attrPersonArray = { "uid", "userPassword", "displayName",
+				"cn", "sn", "mail" };
+		SearchControls searchControls = new SearchControls();
+
+		searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+		searchControls.setSearchScope(SearchControls.OBJECT_SCOPE);
+		// 设置将被返回的Attribute, 可以不设置
+		searchControls.setReturningAttributes(attrPersonArray);
+		// 三个参数分别为：
+		// 上下文；
+		// 要搜索的属性，如果为空或 null，则返回目标上下文中的所有对象；
+		// 控制搜索的搜索控件，如果为 null，则使用默认的搜索控件
+		NamingEnumeration<SearchResult> searchResults = newCtx.search("o=org2",
+				filter, searchControls);
+		// 输出查到的数据
+		while (searchResults.hasMore()) {
+			SearchResult result = searchResults.next();
+			System.out.println(result.getName() + "'s attribute:");
+			NamingEnumeration<? extends Attribute> attrs = result
+					.getAttributes().getAll();
+			while (attrs.hasMore()) {
+				Attribute attr = attrs.next();
+				System.out.println(attr.getID() + "=" + attr.get());
+			}
+			System.out.println("============");
+		}
+	}
+
+	@Test
+	public void canAuthenticate() {
+		String usr = "zsorg1";
+		String pwd = "1";
+		LdapContext ctx = null;
+		try {
+			ctx = LdapHelper.getCtx();
+			SearchControls constraints = new SearchControls();
+			constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+			// constraints.setSearchScope(SearchControls.ONELEVEL_SCOPE);
+			NamingEnumeration<SearchResult> en = ctx.search("", "cn=" + usr,
+					constraints); //
+			// 查询所有用户
+			while (en != null && en.hasMoreElements()) {
+				SearchResult si = en.nextElement();
+				System.out.println("name:   " + si.getName());
+				Attributes attrs = si.getAttributes();
+				if (attrs == null) {
+					System.out.println("No   attributes");
+				} else {
+					Attribute attr = attrs.get("userPassword");
+					Object o = attr.get();
+					byte[] s = (byte[]) o;
+					String pwd2 = new String(s);
+					boolean result = LdapHelper.verifySHA(pwd2, pwd);
+					Assert.assertTrue(result);
+					return;
+				}
+				Assert.assertTrue(false);
+			}
+			ctx.close();
+		} catch (NoSuchAlgorithmException ex) {
+			try {
+				if (ctx != null) {
+					ctx.close();
+				}
+			} catch (NamingException namingException) {
+				namingException.printStackTrace();
+			}
+		} catch (NamingException ex) {
+			try {
+				if (ctx != null) {
+					ctx.close();
+				}
+			} catch (NamingException namingException) {
+				namingException.printStackTrace();
+			}
+		}
+		return;
+	}
+}
