4e972f4c1b84fa2e7ce7ae1bf257fb2d716772d7
wwwcomy <wwwcomy@gmail.com>
2016-01-25 21:49:25 +0800

    Make all logic dynamic for utf-8 encoding.

--- src/main/java/com/iteye/wwwcomy/test/TestUtf8Encoding.java
+++ src/main/java/com/iteye/wwwcomy/test/TestUtf8Encoding.java
@@ -4,6 +4,8 @@ import java.nio.charset.Charset;
 
 /**
  * https://www.zhihu.com/question/23374078
+ * UTF-8 encoding test, given one UTF8
+ * code, return mapping character.
  * 
  * @author wwwcomy
  *
@@ -13,7 +15,7 @@ public class TestUtf8Encoding {
         // "报"的UTF8编码表中对应的编码
         String a = "62a5";
         String binary = Integer.toBinaryString(Integer.valueOf(a, 16));
-        System.out.println(binary);
+        System.out.println("To binary:" + binary);
         // 转成2进制后,根据UTF-8的编码规则进行编码，生成一下二进制码
         // 编码规则：
         // 0xxxxxxx
@@ -23,25 +25,10 @@ public class TestUtf8Encoding {
         // 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
         // 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
         String bi = getUtf8Binary(binary);
-        // 生成二进制码后，按照每8个位一个字节的方式，生成3个byte,进而生成String，可以看到对应的"报"打印出来
-        String a1 = bi.substring(0, 8);
-        String a2 = bi.substring(8, 16);
-        String a3 = bi.substring(16);
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
-        byte[] bs = new byte[3];
+        System.out.println("After UTF-9 encoding:" + bi);
 
-        // 这里从二进制码转换成byte貌似在java里面挺麻烦，如果直接使用Byte.parseByte()方法,会产生溢出，所以要先转成Int然后强转成byte才能生成正常的byte
-        // 注意java里面byte是-128~127，
-        // 八位中的第一位是符号，而int是4个字节，如果1个字节的话，区间就成了0-255了，所以会溢出
-        byte v1 = (byte) Integer.parseInt(a1, 2);
-        byte v2 = (byte) Integer.parseInt(a2, 2);
-        byte v3 = (byte) Integer.parseInt(a3, 2);
-        bs[0] = v1;
-        bs[1] = v2;
-        bs[2] = v3;
-        System.out.println(new String(bs, Charset.forName("UTF-8")));
+        String result = generateResult(bi);
+        System.out.println("Result is:" + result);
     }
 
     /**
@@ -51,8 +38,7 @@ public class TestUtf8Encoding {
      * @return
      */
     private static String getUtf8Binary(String initBinary) {
-        char[] c = initBinary.toCharArray();
-        int len = c.length;
+        int len = initBinary.length();
         StringBuilder sb = new StringBuilder();
         if (len <= 7) {
             sb.append("0");
@@ -63,10 +49,42 @@ public class TestUtf8Encoding {
         } else {
             int unicodeHolder = len / 6;
             for (int i = 0; i < unicodeHolder; i++) {
-                //sb.append(c)
-                // TODO continue this
+                sb.insert(0, "10" + initBinary.substring(len - 6 * (i + 1), len - 6 * (i + 1) + 6));
+            }
+            sb.insert(0, initBinary.substring(0, len % 6));
+            // 这是最高位要补0的个数，假如除以六得到2，即肯定需要2个字节了，那么前面需要"11"，再加上"10"，再减去除以6的余数
+            int remainder = 8 - unicodeHolder - 2 - len % 6;
+            for (int i = 0; i < remainder; i++) {
+                sb.insert(0, "0");
+            }
+            sb.insert(0, "10");
+            for (int i = 0; i < unicodeHolder; i++) {
+                sb.insert(0, "1");
             }
         }
         return sb.toString();
     }
+
+    /**
+     * 按照解码方式生成字符,也是个动态的
+     * 
+     * @param bi
+     * @return
+     */
+    private static String generateResult(String bi) {
+        // 生成二进制码后，按照每8个位一个字节的方式，生成N个byte,进而生成String，可以看到对应的字符打印出来
+        int len = bi.length();
+        int size = len / 8;
+        byte[] bs = new byte[size];
+        for (int i = 0; i < size; i++) {
+            String s = bi.substring(8 * i, 8 * (i + 1));
+            // 这里从二进制码转换成byte貌似在java里面挺麻烦，如果直接使用Byte.parseByte()方法,会产生溢出，所以要先转成Int然后强转成byte才能生成正常的byte
+            // 注意java里面byte是-128~127，
+            // 八位中的第一位是符号，而int是4个字节，如果1个字节的话，区间就成了0-255了，所以会溢出
+            bs[i] = (byte) Integer.parseInt(s, 2);
+        }
+        String result = new String(bs, Charset.forName("UTF-8"));
+        return result;
+    }
+
 }
