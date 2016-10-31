85664eea3e7067b6aa441e2186c4294e76c5f381
wwwcomy <wwwcomy@gmail.com>
2016-10-31 15:08:29 +0800

    Update comment on NIO

--- src/main/java/com/iteye/wwwcomy/lxn/nio/NioEchoClient.java
+++ src/main/java/com/iteye/wwwcomy/lxn/nio/NioEchoClient.java
@@ -15,51 +15,53 @@ public class NioEchoClient {
 
 	public static void main(String[] args) throws IOException {
 		Selector selector = Selector.open();
-		
+
 		SocketChannel sc = SocketChannel.open();
 		sc.configureBlocking(false);
 		sc.connect(new InetSocketAddress(7777));
 		sc.register(selector, SelectionKey.OP_CONNECT);
-		
+
 		BufferedReader stdReader = new BufferedReader(new InputStreamReader(System.in));
 		String line;
-		
+
 		Iterator<SelectionKey> keys;
 		SelectionKey key;
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		int len;
-		while(selector.select() >= 0) {
+		while (selector.select() >= 0) {
 			keys = selector.selectedKeys().iterator();
-			while(keys.hasNext()) {
+			while (keys.hasNext()) {
 				key = keys.next();
 				keys.remove();
-				
-				if(key.isConnectable()) {
+
+				if (key.isConnectable()) {
 					SocketChannel sc2 = (SocketChannel) key.channel();
-					if(sc2.isConnectionPending()) {
+					if (sc2.isConnectionPending()) {
 						sc2.finishConnect();
 					}
 					key.interestOps(SelectionKey.OP_WRITE);
 				}
-				if(key.isWritable()) {
+				if (key.isWritable()) {
 					// block here
 					System.out.println("Please input something, \"exit\" to return.");
-					if((line = stdReader.readLine()) != null) {
+					if ((line = stdReader.readLine()) != null) {
 						SocketChannel sc2 = (SocketChannel) key.channel();
-						if(line.equals("exit")) {
+						if (line.equals("exit")) {
 							sc2.close();
 							System.exit(0);
 							return;
 						}
 						sc2.write(ByteBuffer.wrap(line.getBytes()));
 						key.interestOps(SelectionKey.OP_READ);
+						// 这里即使加上 Thread.sleep(3000);
+						// 也不会影响console的输入,怀疑是console的输入是在另外一个线程的，加上之后可以看到server端还是会等待几秒才能收到console中输入的内容
 					}
 				}
-				if(key.isReadable()) {
+				if (key.isReadable()) {
 					SocketChannel sc2 = (SocketChannel) key.channel();
 					ByteArrayOutputStream baos = new ByteArrayOutputStream();
 					buffer.clear();
-					while((len = sc2.read(buffer)) > 0) {
+					while ((len = sc2.read(buffer)) > 0) {
 						buffer.flip();
 						baos.write(buffer.array(), 0, len);
 					}
--- src/main/java/com/iteye/wwwcomy/lxn/nio/NioEchoServer.java
+++ src/main/java/com/iteye/wwwcomy/lxn/nio/NioEchoServer.java
@@ -1,7 +1,6 @@
 package com.iteye.wwwcomy.lxn.nio;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
@@ -10,56 +9,66 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
-
 public class NioEchoServer {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		Selector selector = Selector.open();
 		ServerSocketChannel ssc = ServerSocketChannel.open();
+		// 必须配置阻塞模式为False
 		ssc.configureBlocking(false);
 		ssc.socket().bind(new InetSocketAddress(7777));
+		// 注册感兴趣的事件，这里注册的是接受事件
 		ssc.register(selector, SelectionKey.OP_ACCEPT);
-		
+
 		Iterator<SelectionKey> keys;
 		SelectionKey key;
-		while(selector.select() >= 0) {
+		// 阻塞调用线程，直到有某个Channel的某个感兴趣的Op准备好(单个Selector可以注册多个Channel)
+		while (selector.select() >= 0) {
 			keys = selector.selectedKeys().iterator();
-			while(keys.hasNext()) {
+			while (keys.hasNext()) {
 				key = keys.next();
 				keys.remove();
-				if(key.isAcceptable()) {
+				if (key.isAcceptable()) {
 					ServerSocketChannel ssc2 = (ServerSocketChannel) key.channel();
 					SocketChannel sc = ssc2.accept();
 					sc.configureBlocking(false);
 					sc.register(selector, SelectionKey.OP_READ);
 					System.out.println("Accept new connection: " + sc.socket().getPort());
 				}
-				if(key.isReadable()) {
-					SocketChannel sc = (SocketChannel) key.channel();
-					ByteBuffer buffer = ByteBuffer.allocate(1024);
-					ByteArrayOutputStream baos = new ByteArrayOutputStream();
-					int len;
-					while((len=sc.read(buffer)) > 0) {
-						baos.write(buffer.array(), 0, len);
-					}
-					if(len == -1){
-						sc.close();
-						System.out.println("Connection closed: " + sc.socket().getPort());
-						continue;
-					}
-					System.out.println("Receive data from " + sc.socket().getPort() + ": " + baos.toString());
-					key.attach(buffer);
-					key.interestOps(SelectionKey.OP_WRITE);
+				if (key.isReadable()) {
+					readDataFromSocket(key);
 				}
-				if(key.isWritable()) {
-					SocketChannel sc = (SocketChannel) key.channel();
-					ByteBuffer buffer = (ByteBuffer) key.attachment();
-					buffer.flip();
-					sc.write(buffer);
-					key.interestOps(SelectionKey.OP_READ);
+				if (key.isWritable()) {
+					writeDataThroughSocket(key);
 				}
 			}
 		}
+	}
+
+	private static void readDataFromSocket(SelectionKey key) throws Exception {
+		SocketChannel sc = (SocketChannel) key.channel();
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int len;
+		while ((len = sc.read(buffer)) > 0) {
+			baos.write(buffer.array(), 0, len);
+		}
+		if (len == -1) {
+			sc.close();
+			System.out.println("Connection closed: " + sc.socket().getPort());
+			return;
+		}
+		System.out.println("Receive data from " + sc.socket().getPort() + ": " + baos.toString());
+		key.attach(buffer);
+		key.interestOps(SelectionKey.OP_WRITE);
+	}
+
+	private static void writeDataThroughSocket(SelectionKey key) throws Exception {
+		SocketChannel sc = (SocketChannel) key.channel();
+		ByteBuffer buffer = (ByteBuffer) key.attachment();
+		buffer.flip();
+		sc.write(buffer);
+		key.interestOps(SelectionKey.OP_READ);
 	}
 
 }
