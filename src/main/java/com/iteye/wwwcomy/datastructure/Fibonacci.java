25c1bb347bdd3c8d58ac6edbdea4fdff0501aeb0
wwwcomy <wwwcomy@gmail.com>
2017-02-24 16:35:01 +0800

    Add Fibonacci tail call

--- src/main/java/com/iteye/wwwcomy/datastructure/Fibonacci.java
+++ src/main/java/com/iteye/wwwcomy/datastructure/Fibonacci.java
@@ -11,7 +11,7 @@ package com.iteye.wwwcomy.datastructure;
 public class Fibonacci {
 
 	static int fibonacci1(int x) {
-		System.out.println("Method 1 called");
+		// System.out.println("Method 1 called");
 		if (x <= 2) {
 			return 1;
 		}
@@ -26,7 +26,7 @@ public class Fibonacci {
 	}
 
 	static int fibonacci2(int x) {
-		System.out.println("Method 2 called");
+		// System.out.println("Method 2 called");
 		if (x <= 2 || CACHE[x - 1] != 0) {
 			return CACHE[x - 1];
 		}
@@ -34,8 +34,18 @@ public class Fibonacci {
 		return CACHE[x - 1];
 	}
 
+	static int fibonacciTailCall(int x, int y, int n) {
+		// System.out.println("Method fibonacciTailCall called");
+		if (n == 0) {
+			return x;
+		}
+
+		return fibonacciTailCall(x + y, x, n - 1);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(fibonacci1(6));
 		System.out.println(fibonacci2(6));
+		System.out.println(fibonacciTailCall(1, 0, 6));
 	}
 }
