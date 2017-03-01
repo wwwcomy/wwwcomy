26b9dfae41871a385a5aae887ca791fd2a3df8b8
wwwcomy <wwwcomy@gmail.com>
2017-03-01 15:03:37 +0800

    Several ways of implementing Fibonacci

--- src/main/java/com/iteye/wwwcomy/datastructure/Fibonacci.java
+++ src/main/java/com/iteye/wwwcomy/datastructure/Fibonacci.java
@@ -34,18 +34,44 @@ public class Fibonacci {
 		return CACHE[x - 1];
 	}
 
+	/**
+	 * This is a very tricky way of changing the loop to a recursive pattern,
+	 * and of cause, this is a tail call.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param n
+	 * @return
+	 */
 	static int fibonacciTailCall(int x, int y, int n) {
 		// System.out.println("Method fibonacciTailCall called");
-		if (n == 0) {
+		if (n == 1) {
 			return x;
 		}
 
 		return fibonacciTailCall(x + y, x, n - 1);
 	}
 
+	static int fibonacciUseWhile(int x) {
+		if (x < 3) {
+			return 1;
+		}
+		int num1 = 1;
+		int num2 = 1;
+		int num3 = 0;
+		while (x > 2) {
+			num3 = num2;
+			num2 = num1 + num2;
+			num1 = num3;
+			x--;
+		}
+		return num2;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(fibonacci1(6));
 		System.out.println(fibonacci2(6));
 		System.out.println(fibonacciTailCall(1, 0, 6));
+		System.out.println(fibonacciUseWhile(6));
 	}
 }
